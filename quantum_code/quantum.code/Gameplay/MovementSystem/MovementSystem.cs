fe13bb9fd58ed048f5dc70b138ac38b22c5a913c
goldfinch <dyboshit@gmail.com>
2024-10-07 15:21:36 +0300

    feat: movement system

---
+++ quantum_code/quantum.code/Gameplay/MovementSystem/MovementSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Photon.Deterministic;
+
+namespace Quantum.Gameplay.MovementSystem;
+
+public unsafe class MovementSystem : SystemMainThreadFilter<MovementSystem.Filter> {
+
+    public struct Filter {
+        public EntityRef Entity;
+        public Movement* Movement;
+        public Transform3D* Transform;
+    }
+    
+    public override void Update(Frame frame, ref Filter filter) {
+        FPVector3 newPosition = CalculateNewPosition(filter.Transform->Position, filter.Movement->Target, filter.Movement->Speed);
+        filter.Transform->Position = newPosition;
+    }
+
+    private FPVector3 CalculateNewPosition(FPVector3 transformPosition, FPVector3 movementTarget, FP movementSpeed) {
+        return FPVector3.Lerp(transformPosition, movementTarget, movementSpeed);
+    }
+    
+}
--- quantum_code/quantum.code/SystemSetup.cs
+++ quantum_code/quantum.code/SystemSetup.cs
@@ -1,27 +1,24 @@
-﻿using Photon.Deterministic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Quantum.Gameplay.MovementSystem;
 
 namespace Quantum {
   public static class SystemSetup {
     public static SystemBase[] CreateSystems(RuntimeConfig gameConfig, SimulationConfig simulationConfig) {
       return new SystemBase[] {
         // pre-defined core systems
-        new Core.CullingSystem2D(), 
+        //new Core.CullingSystem2D(), 
         new Core.CullingSystem3D(),
         
-        new Core.PhysicsSystem2D(),
+        //new Core.PhysicsSystem2D(),
         new Core.PhysicsSystem3D(),
 
         Core.DebugCommand.CreateSystem(),
 
-        new Core.NavigationSystem(),
+        //new Core.NavigationSystem(),
         new Core.EntityPrototypeSystem(),
         new Core.PlayerConnectedSystem(),
 
         // user systems go here 
+        new MovementSystem(),
       };
     }
   }
