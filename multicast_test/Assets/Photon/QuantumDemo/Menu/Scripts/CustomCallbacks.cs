97eb47bbe80e234ce406e8a85723044a0d8947e1
goldfinch <dyboshit@gmail.com>
2024-10-08 13:08:27 +0300

    feat: multiplayer connection

--- multicast_test/Assets/Photon/QuantumDemo/Menu/Scripts/CustomCallbacks.cs
+++ multicast_test/Assets/Photon/QuantumDemo/Menu/Scripts/CustomCallbacks.cs
@@ -1,18 +1,22 @@
-﻿using UnityEngine;
+﻿using Quantum;
+using UnityEngine;
 
 public class CustomCallbacks : QuantumCallbacks {
 
-  public override void OnGameStart(Quantum.QuantumGame game) {
-    // paused on Start means waiting for Snapshot
-    if (game.Session.IsPaused) return;
+  [SerializeField] private RuntimePlayer player;
 
-    foreach (var lp in game.GetLocalPlayers()) {
-      Debug.Log("CustomCallbacks - sending player: " + lp);
-      game.SendPlayerData(lp, new Quantum.RuntimePlayer { });
+  public override void OnGameStart(QuantumGame game) {
+    if (game.Session.IsPaused) {
+      return;
+    }
+
+    foreach (int localPlayer in game.GetLocalPlayers()) {
+      Debug.Log("CustomCallbacks - sending player: " + localPlayer);
+      game.SendPlayerData(localPlayer, player);
     }
   }
 
-  public override void OnGameResync(Quantum.QuantumGame game)
+  public override void OnGameResync(QuantumGame game)
   {
     Debug.Log("Detected Resync. Verified tick: " + game.Frames.Verified.Number);
   }
